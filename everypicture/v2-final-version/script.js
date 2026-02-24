0fd954974ba0bf0ebca4c20ebd141b29b08f4140
Shu Yin Yu <syiyu@ucdavis.edu>
2026-02-23 18:44:09 -0800

    everypicture

--- everypicture/v2-final-version/script.js
+++ everypicture/v2-final-version/script.js
@@ -4,11 +4,12 @@
 
     // const open = document.querySelector('.open');
 
+    // when the image is clicked, the overlay shows
     document.querySelector('.open').addEventListener('click', function(event){
         event.preventDefault();
         document.querySelector('#overlay').className = 'showing';
     });
-
+    // when image is closed by the x button, the overlay is hidden
     document.querySelector('.close').addEventListener('click', function(){
         event.preventDefault();
         document.querySelector('#overlay').className = 'hidden';
@@ -20,9 +21,9 @@
         document.querySelector('#overlay2').className = 'showing2';
     });
 
-    document.querySelector('.close2').addEventListener('click', function(){
+    document.querySelector('#close2').addEventListener('click', function(){
         event.preventDefault();
-        document.querySelector('#overlay2').className = 'hidden2';
+        document.querySelector('#overlay2').className = 'hidden';
     });
 
 
@@ -31,9 +32,9 @@
         document.querySelector('#overlay3').className = 'showing3';
     });
 
-    document.querySelector('.close3').addEventListener('click', function(){
+    document.querySelector('#close3').addEventListener('click', function(){
         event.preventDefault();
-        document.querySelector('#overlay3').className = 'hidden3';
+        document.querySelector('#overlay3').className = 'hidden';
     });
 
 
@@ -43,9 +44,9 @@
         document.querySelector('#overlay4').className = 'showing4';
     });
 
-    document.querySelector('.close4').addEventListener('click', function(){
+    document.querySelector('#close4').addEventListener('click', function(){
         event.preventDefault();
-        document.querySelector('#overlay4').className = 'hidden4';
+        document.querySelector('#overlay4').className = 'hidden';
     });
 
 
@@ -55,9 +56,9 @@
         document.querySelector('#overlay5').className = 'showing5';
     });
 
-    document.querySelector('.close5').addEventListener('click', function(){
+    document.querySelector('#close5').addEventListener('click', function(){
         event.preventDefault();
-        document.querySelector('#overlay5').className = 'hidden5';
+        document.querySelector('#overlay5').className = 'hidden';
     });
     
 })();
