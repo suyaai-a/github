cdd6fd278d8b7e49ef87f78840f89f357a8481a2
Shu Yin Yu <syu118@scc2102-09.ucdavis.edu>
2026-03-10 16:57:12 -0700

    gameon

--- game/v1-html-css-only/script.js
+++ game/v1-html-css-only/script.js
@@ -7,15 +7,50 @@
     const start = document.querySelector('#start');
     const player1score = document.querySelector('#score1 p');
     const flip1 = document.querySelector('#flip1');
+    const overlay = document.querySelector('#overlaymsg p')
+
+    const gameData = {
+        deck: [
+            {img: "card1.png", value: 1},
+            {img: "card2.png", value: 2},
+            {img: "card3.png", value: 3},
+            {img: "card4.png", value: 4},
+            {img: "card5.png", value: 5},
+            {img: "card6.png", value: 6},
+        ],
+        players: ['player 1', 'player 2'],
+        score: [0, 0],
+        shuffle1: 0,
+        shuffle2: 0,
+        rollSum: 0,
+        index: 0,
+        gameEnd: 29
+
+    };
 
     start.addEventListener('click', function() {
         homepage.className = 'hidden';
         playscreen.className = 'showing';
+
+        
     });
 
+    // function setUpTurn(){
+    //     overlay.innerHTML = `<p>Player ${gameData.players[gameData.index]}'s turn! </p>`
+    //     setTimeout(setUpTurn, 2000);
+    // }
+
     flip1.addEventListener('click', function(){
-        player1score.innerHTML = 'Good luck!!';
+        
+
+        player1score.innerHTML = `<p>Player 1:<br> ${gameData.players[gameData.index]}</p>`;
+        playscreen.innerHTML += `<img src="images/${gameData.deck[gameData.shuffle1-1]}"> <img src="images/${gameData.deck[gameData.shuffle2-1]}">`;
+        throwDice();
     });
+
+    function throwDice(){
+        gameData.rollSum = gameData.shuffle1 + gameData.shuffle2;
+    }
 
     
 })();
