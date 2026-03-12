c65de3e268d1d623cac18698a76b902d4c049118
Shu Yin Yu <syiyu@ucdavis.edu>
2026-03-11 17:15:53 -0700

    gameon

--- game/v1-html-css-only/script.js
+++ game/v1-html-css-only/script.js
@@ -7,15 +7,84 @@
     const start = document.querySelector('#start');
     const player1score = document.querySelector('#score1 p');
     const flip1 = document.querySelector('#flip1');
+    const overlay = document.querySelector('#overlaymsg p')
+    const firstcard = document.querySelector('#firstcard');
+    const secondcard = document.querySelector('#secondcard');
+    const thirdcard = document.querySelector('#thirdcard');
+    const fourthcard = document.querySelector('#fourthcard');
+    const card1 = document.querySelector('#card1');
+    const card2 = document.querySelector('#card2');
+
+    const gameData = {
+        deck: [
+            {img: "card1.png", value: 1},
+            {img: "card2.png", value: 2},
+            {img: "card3.png", value: 3},
+            {img: "card4.png", value: 4},
+            {img: "card5.png", value: 5},
+            {img: "card6.png", value: 6},
+        ],
+        players: ['player 1', 'player 2'],
+        score: [0, 0],
+        shuffle1: 0,
+        shuffle2: 0,
+        sum: 0,
+        index: 0, 
+        // index: which player turn it is
+        gameEnd: 29
+
+    };
 
     start.addEventListener('click', function() {
         homepage.className = 'hidden';
         playscreen.className = 'showing';
+        
+
+        gameData.index = Math.round(Math.random());
+        console.log(gameData.index);
+        setUpTurn();
+        // displayPopUp(message)
     });
 
+    // function displayPopUp(message){
+    //     document.querySelector('#overlaymsg').innerHTML=`<p>${}</p>`
+    // }
+
+
+    function setUpTurn(){
+        overlay.innerHTML = `${gameData.players[gameData.index]}'s turn!`;
+        document.querySelector('#message').classList = 'showing';
+        setTimeout(setUpTurn, 2000);
+
+        setTimeout(function(){
+            document.querySelector('#message').classList = 'hidden';
+        },2000);
+
+        shuffleCard();
+    }
+
+    function shuffleCard(){
+        gameData.shuffle1 = Math.floor(Math.random()* 6) + 1;
+        gameData.shuffle2 = Math.floor(Math.random()* 6) + 1;
+        // console.log(gameData.roll1, gameData.roll2);
+        // gameData.roll1 = 1;
+        // gameData.roll2 = 1;
+
+        overlay.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        card1.innerHTML += `<img src="images/${gameData.dice[gameData.shuffle2-1]}"> <img src="images/${gameData.dice[gameData.shuffle2-1]}">`;
+        card2.innerHTML += `<img src="images/${gameData.dice[gameData.shuffle2-1]}"> <img src="images/${gameData.dice[gameData.shuffle2-1]}">`;
+        gameData.rollSum = gameData.roll1 + gameData.roll2;
+    }
+
     flip1.addEventListener('click', function(){
-        player1score.innerHTML = 'Good luck!!';
+        player1score.innerHTML = `<p>Player 1:<br> ${gameData.score[gameData.index]}</p>`;
+        playscreen.innerHTML += `<img src="images/${gameData.deck[gameData.shuffle1-1]}"> <img src="images/${gameData.deck[gameData.shuffle2-1]}">`;
+        throwDice();
     });
+
+    function throwDice(){
+        gameData.rollSum = gameData.shuffle1 + gameData.shuffle2;
+    }
 
     
 })();
